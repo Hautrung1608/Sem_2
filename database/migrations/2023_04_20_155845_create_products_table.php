0287a8f032e4bf2a3a9169d52ba21bc960aaa460
NGuyen_Viet_Duc <105768873+xin-chao-the-gioi@users.noreply.github.com>
2023-04-26 10:42:14 +0700

    hehe3

--- database/migrations/2023_04_20_155845_create_products_table.php
+++ database/migrations/2023_04_20_155845_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->bigInteger("cate_id")->unsigned();
+            $table->string("unit");
+            $table->string("origin");
+            $table->foreign('cate_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
--- database/migrations/2023_04_21_030504_create_acounts_table.php
+++ database/migrations/2023_04_21_030504_create_acounts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('acounts', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("image");
+            $table->string("email");
+            $table->string("pasword");
+            $table->tinyInteger("status");
             $table->timestamps();
         });
     }
