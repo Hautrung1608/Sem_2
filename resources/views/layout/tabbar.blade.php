00e24a3678a826abb5bec4191f02d0086bc2565d
NGuyen_Viet_Duc <105768873+xin-chao-the-gioi@users.noreply.github.com>
2023-05-16 12:34:35 +0700

    hehe

--- resources/views/layout/tabbar.blade.php
+++ resources/views/layout/tabbar.blade.php
@@ -1,15 +1,15 @@
-<div class="col-md-2 text-center">
+<div class="col-md-2 text-center div-bg">
     <div class="">
-      <div class="container btn btn-outline-primary nut">
+      <div class="container nut">
         <a class="chu" href="{{route('category.index')}}"> Category</a>
       </div>
-      <div class="container btn btn-outline-primary nut">
+      <div class="container nut">
         <a class="chu" href="{{route('product.index')}}">Product</a>
       </div>
-      <div class="container btn btn-outline-primary nut">
-        <a class="chu" href=""></a>
+      <div class="container nut">
+        <a class="chu" href="">hehe</a>
       </div>
-      <div class="container btn btn-outline-primary nut">
+      <div class="container nut">
         <a class="chu" href=""> hehe</a>
       </div>
     </div>
